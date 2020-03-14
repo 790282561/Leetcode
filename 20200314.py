84478ac242718dcb9aac04ac1d798de628d26cd1
zhangjun <790282561@qq.com>
2020-03-15 00:40:52 +0800

    20200314，数字反转

--- 20200314.py
+++ 20200314.py
@@ -21,5 +21,40 @@ class Solution:
         return res if x > 0 else -res
 
 # example = Solution().reverse(-123)
-example = Solution().reverse_cacu(-543)
-print(example)
+# example = Solution().reverse_cacu(-543)
+# print(example)
+
+# no.9 回文数
+
+class Solution:
+    # 全返
+    def isPalindrome(self, x: int) -> bool:
+        if x > 0:
+            y, res = x, 0
+            while y != 0:
+                res = res * 10 + y % 10
+                y //= 10
+            return True if res == x else False
+        elif x <= 0:
+            return True if x == 0 else False
+
+    # 半反
+    def isPalindrome_half(self, x: int) -> bool:
+        if x < 0:
+            return False
+        elif x == 0:
+            return True
+        elif x % 10 == 0:
+            return False
+        else:
+            res = 0
+            while x > res:
+                res = res * 10 + x % 10
+                x //= 10
+            if x == res or res // 10 == x:
+                return True
+            else:
+                return False
+
+example = Solution()
+print(example.isPalindrome_half(0))
